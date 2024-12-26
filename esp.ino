358f835c091e3cd0991f181320e0962c3350608d
Juzo(-.-) <alexs22221@gmail.com>
2024-12-26 08:50:47 +0300

    new

--- esp.ino
+++ esp.ino
@@ -1,52 +1,78 @@
 #include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <WiFiClient.h>
 #include <ArduinoJson.h>
+
 
 #include "DHT.h"
 #define DHTTYPE DHT11
 
 
+// вписываем здесь SSID и пароль для вашей WiFi-сети:
+// const char* ssid = "Kiski_muriski";
+// const char* password = "eY7m76S_rP";
+const char* ssid = "iPhone (Alexey)";
+const char* password = "12346789";
 
-// вписываем здесь SSID и пароль для вашей WiFi-сети: 
-const char* ssid = "";
-const char* password = "";
+const char* serverName = "http://172.20.10.9:8000/";
+WiFiClient wifiClient;
 
 
 const uint8_t DHTPin = D7;
 DHT dht(DHTPin, DHTTYPE);
 
 
-float Temperature;
-float Humidity;
-
 void setup() {
   Serial.begin(115200);
-  dht.begin(); 
+  dht.begin();
 
+  // WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
- 
+
   Serial.println("Подключение к WiFi");
- 
-  while(WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
   }
- 
+
   Serial.println("");
   Serial.print("Подключение успешно, ip=");
   Serial.println(WiFi.localIP());
- 
-  // Отправляем сообщение в WhatsApp
-  Serial.print("Привет от ESP!\n");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   float temp = dht.readTemperature();
   float hum = dht.readHumidity();
-  Serial.print(temp);
-  Serial.print(' ');
-  Serial.print(hum);
-  Serial.print('\n');
+  Serial.printf("%.2f %.2f \n", temp, hum);
+
+  HTTPClient http;
+  http.begin(wifiClient, serverName);
+  http.addHeader("Content-Type", "application/json");
+
+  DynamicJsonDocument doc(512);
+  JsonObject object = doc.to<JsonObject>();
+  object["temperature"] = temp;
+  object["humidity"] = hum;
+
+  String data;
+  serializeJsonPretty(object, data);
+  Serial.println(data);
+
+  int httpCode = http.POST(data);
+
+  if (httpCode == 200) {
+    Serial.println("POST succeeded with code:");
+    Serial.println(httpCode);
+
+  } else if (httpCode != 200) {
+    Serial.println("POST failed with code:");
+    Serial.println(httpCode);
+
+  } else {
+    Serial.println("Unknown error");
+  }
+  http.end();
   delay(5000);
 }
-
